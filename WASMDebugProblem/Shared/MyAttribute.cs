90b04761ccd407ca7963d37e73163b65021f1575
Jonas Sjöhag <jonas.sjohag@paytrim.com>
2021-04-13 15:10:57 +0200

    Add project files.

---
+++ WASMDebugProblem/Shared/MyAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASMDebugProblem.Shared
+{
+    [System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    public sealed class MyAttribute : Attribute
+    {
+        readonly string positionalString;
+        public MyAttribute(string positionalString)
+        {
+            this.positionalString = positionalString;
+        }
+        public MyAttribute(MyEnum myEnum)
+        {
+            this.positionalString = Enum.GetName(myEnum);
+        }
+        public string PositionalString
+        {
+            get { return positionalString; }
+        }
+    }
+    public enum MyEnum
+    {
+        One, Two, Three
+    }
+}
